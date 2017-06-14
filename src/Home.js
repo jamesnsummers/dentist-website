5c7811155e41f4ae2aab96b4022be0f47898a834
James Summers <jamesnsummers@gmail.com>
2017-06-14 14:29:57 -0500

    initial commit

---
+++ src/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="Home">
+        <h1>Dentist Website</h1>
+        <p>I clean teeth, like, a lot.</p>
+      </div>
+    );
+  }
+}
+
+export default Home;
